b46bc855e918cab44436299d5571359e59530a86
Rex <s20063196@163.com>
2016-05-18 11:17:56 +0800

    add capture function for org note

--- emacs.d/lisp/s-org.el
+++ emacs.d/lisp/s-org.el
@@ -21,3 +21,34 @@
 ;; set org-agenda 
 (global-set-key (kbd "C-c a") 'org-agenda)
 
+;; disable `validate'
+(setq org-html-validation-link nil)
+
+;; disable creator info
+(setq org-export-with-creator nil)
+
+;; disable author info
+(setq org-export-with-author nil)
+
+;; disable time stamp
+(setq org-export-time-stamp-file nil)
+
+
+
+;; === screenshot in org-mode
+;; require: scrot installed
+(defun s-screenshot()
+  "take a screenshot into a unique-named file in the current buffer file
+   directory and insert a link to this file."
+  (interactive)
+  (setq filename
+		(concat(make-temp-name
+				(concat(getenv "HOME") "/Pictures/"))".png"))
+  (suspend-frame)
+  (call-process-shell-command "scrot" nil nil nil nil "-s" (concat
+															"\""filename"\""))
+  (insert (concat "[[" filename "]]"))
+  ;(org-display-inline-images)
+  )
+
+(global-set-key (kbd "M-p") 's-screenshot)
