1ceafe28769982a7fa11154c288754270363f346
Rex <s20063196@163.com>
2016-06-06 13:39:24 +0800

    speed up emacs start

--- init.el
+++ init.el
@@ -1,14 +1,19 @@
-(setq emacs-load-start-time (current-time))
+(defvar best-gc-cons-threshold 4000000 "Best default gc threshold value. Should't be too big.")
+;; don't GC during startup to save time
+(setq gc-cons-threshold most-positive-fixnum)
 
+(setq emacs-load-start-time (current-time))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
 ;; load els
-(load "s-ui.el")
-(load "s-org.el")
-(load "s-code.el")
-(load "s-packages.el")
-(load "s-keybindings.el")
+(let ((file-name-handler-alist nil))
+  (load "s-ui.el")
+  (load "s-org.el")
+  (load "s-code.el")
+  (load "s-packages.el")
+  (load "s-keybindings.el")
 
-(when (require 'time-date nil t)
-  (message "Emacs startup time: %d seconds."
-		   (time-to-seconds (time-since emacs-load-start-time))))
+  (when (require 'time-date nil t)
+	(message "Emacs startup time: %d seconds."
+			 (time-to-seconds (time-since emacs-load-start-time))))
+  )
