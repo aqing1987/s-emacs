1a43e6b207802a78e1b1cf7adba989761ad43e8c
Rex <s20063196@163.com>
2016-06-06 13:31:06 +0800

    add load time info

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(setq emacs-load-start-time (current-time))
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
 ;; load els
@@ -6,3 +8,7 @@
 (load "s-code.el")
 (load "s-packages.el")
 (load "s-keybindings.el")
+
+(when (require 'time-date nil t)
+  (message "Emacs startup time: %d seconds."
+		   (time-to-seconds (time-since emacs-load-start-time))))
