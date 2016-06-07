8bee5df6e0872f2a065354d83ef282b72f8bcbbc
Rex <s20063196@163.com>
2016-06-07 13:46:18 +0800

    add spell check support

--- init.el
+++ init.el
@@ -5,14 +5,32 @@
 (setq emacs-load-start-time (current-time))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
+;;----------------------------------------------------------------------------
+;; Which functionality to enable (use t or nil for true and false)
+;;----------------------------------------------------------------------------
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *win64* (eq system-type 'windows-nt) )
+(setq *cygwin* (eq system-type 'cygwin) )
+(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
+(setq *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)) )
+(setq *emacs24* (and (not (featurep 'xemacs)) (or (>= emacs-major-version 24))) )
+(setq *no-memory* (cond
+                   (*is-a-mac*
+                    (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
+                   (*linux* nil)
+                   (t nil)))
+
 ;; load els
 (let ((file-name-handler-alist nil))
   (require 'init-modeline)
   (require 'cl-lib)
   (require 'init-utils)
-  (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+  ;; Must come before elpa, as it may provide package.el
+  (require 'init-site-lisp) 
   (require 'init-elpa)
   (require 'init-frame-hooks)
+  ;; any file use flyspell should be initialized after init-spelling
+  (require 'init-spelling)
   (require 'init-yasnippet)
   (load "s-ui.el")
   (load "s-org.el")
