72ec26fc520d1f2cda48e24c3c8b1626ed6251fe
Neil <s20063196@163.com>
2017-03-30 13:44:08 +0800

    update `F2' key open-init-file path

--- lisp/s-keybindings.el
+++ lisp/s-keybindings.el
@@ -4,7 +4,7 @@
 ;; open init file quickly
 (defun open-init-file()
   (interactive)
-  (find-file "~/workspace/open-src/s-emacs/emacs.d/init.el"))
+  (find-file "~/.emacs.d/init.el"))
 
 ;; map function open-init-file() to key <f2>
 (global-set-key (kbd "<f2>") 'open-init-file)
