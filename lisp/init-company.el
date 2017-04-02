17dcf39d2e5a434a041bf3b20d58017a5499b996
Neil <s20063196@163.com>
2017-04-02 21:14:14 +0800

    fix `Symbol's value as variable is void: -' issue

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -11,16 +11,10 @@
 
 (eval-after-load 'company
   '(progn
-     ;; @see https://github.com/company-mode/company-mode/issues/348
-     (unless (featurep 'company-statistics)
-       (require 'company-statistics))
-     (company-statistics-mode)
-
      (add-to-list 'company-backends 'company-cmake)
      (add-to-list 'company-backends 'company-c-headers)
      ;; can't work with TRAMP
      (setq company-backends (delete 'company-ropemacs company-backends))
-     ;; (setq company-backends (delete 'company-capf company-backends))
 
      ;; I don't like the downcase word in company-dabbrev
      ;; for languages use camel case naming convention
@@ -33,8 +27,8 @@
      (setq company-require-match nil)
      (setq company-etags-ignore-case t)
 
-     ;; @see https://github.com/redguardtoo/emacs.d/commit/2ff305c1ddd7faff6dc9fa0869e39f1e9ed1182d
-     (defadvice company-in-string-or-comment (around company-in-string-or-comment-hack activate)
+     (defadvice company-in-string-or-comment
+	 (around company-in-string-or-comment-hack activate)
        ;; you can use (ad-get-arg 0) and (ad-set-arg 0) to tweak the arguments
        (if (memq major-mode '(php-mode html-mode web-mode nxml-mode))
            (setq ad-return-value nil)
