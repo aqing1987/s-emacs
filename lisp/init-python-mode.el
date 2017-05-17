fa1cb7b85d009feb6a9465f304eb322e5267f290
Neil <s20063196@163.com>
2017-05-17 19:07:56 +0800

    python-mode: set offset as 2 spaces

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,5 +1,8 @@
 (autoload 'doctest-mode "doctest-mode" "Python doctest editing mode." t)
 
+(setq python-indent-guess-indent-offset nil)
+(setq python-indent-offset 2)
+
 (setq interpreter-mode-alist
       (cons '("python" . python-mode) interpreter-mode-alist))
 
@@ -8,7 +11,7 @@
     ;; run command `pip install jedi flake8 importmagic` in shell,
     ;; or just check https://github.com/jorgenschaefer/elpy
     (elpy-mode 1)
-    ;; http://emacs.stackexchange.com/questions/3322/python-auto-indent-problem/3338#3338
+    ;; http://emacs.stackexchange.com/questions/3322/python-auto-indent-problem/3338#
     ;; emacs 24.4 only
     (setq electric-indent-chars (delq ?: electric-indent-chars))
     ))
