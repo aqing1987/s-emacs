cfdf7292719f232d68d1af4d14791327d42fc855
Neil <s20063196@163.com>
2017-12-25 10:07:41 +0800

    elpy setup

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,16 +1,12 @@
-(autoload 'doctest-mode "doctest-mode" "Python doctest editing mode." t)
-
-(setq python-indent-guess-indent-offset nil)
-(setq python-indent-offset 2)
-
 (setq interpreter-mode-alist
       (cons '("python" . python-mode) interpreter-mode-alist))
 
+;; run command `pip install jedi flake8 importmagic` in shell,
+;; or just check https://github.com/jorgenschaefer/elpy
+(elpy-enable)
+
 (defun python-mode-hook-setup ()
   (unless (is-buffer-file-temp)
-    ;; run command `pip install jedi flake8 importmagic` in shell,
-    ;; or just check https://github.com/jorgenschaefer/elpy
-    (elpy-mode 1)
     ;; http://emacs.stackexchange.com/questions/3322/python-auto-indent-problem/3338#
     ;; emacs 24.4 only
     (setq electric-indent-chars (delq ?: electric-indent-chars))
